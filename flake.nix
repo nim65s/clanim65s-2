46af0e50a611131d27ded2fdb3dcf7683ce6837a
clan-tool <clan@example.com>
2024-04-28 12:47:08 +0200

    configure fix

--- flake.nix
+++ flake.nix
@@ -59,7 +59,7 @@
 
             # remote> lsblk --output NAME,PTUUID,FSTYPE,SIZE,MOUNTPOINT
             clan.diskLayouts.singleDiskExt4 = {
-              device = "/dev/disk/by-id/__CHANGE_ME__";
+              device = "/dev/disk/by-id/nvme-WD_BLACK_SN770_500GB_23313J803792";
             };
 
             clan.networking.zerotier.networking.enable = true;
